283ad030f283b279fbc662c4994ab5c6514e3a28
acapodanno <alessandro.capodanno95@gmail.com>
2022-02-10 12:55:50 +0100

    Add new project

---
+++ Group6/Group6/Group6App.swift
@@ -0,0 +1,17 @@
+//
+//  Group6App.swift
+//  Group6
+//
+//  Created by Alessandro Capodanno on 10/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Group6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
