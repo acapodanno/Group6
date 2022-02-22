e4f0dc145ff6569d7c9042aac5907da575b03e54
Luigi Verolla <luverolla@gmx.it>
2022-02-22 19:23:03 +0100

    added proximity notifications

---
+++ Group6/Group6/View/NotificationsRouteView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationsRouteView.swift
+//  Group6
+//
+//  Created by Luigi Verolla on 22/02/22.
+//
+
+import SwiftUI
+
+struct NotificationsRouteView: View
+{
+    var body: some View
+    {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationsRouteView_Previews: PreviewProvider
+{
+    static var previews: some View {
+        NotificationsRouteView()
+    }
+}
